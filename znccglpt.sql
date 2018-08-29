a5f115f85f7dacd86c0b591dc716c20a11337d0a
YevgeniC <yevgenicross@gmail.com>
2018-08-29 13:44:04 +0800

    数据库

--- znccglpt.sql
+++ znccglpt.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50722
 File Encoding         : 65001
 
-Date: 2018-08-24 17:54:49
+Date: 2018-08-29 13:43:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,23 +22,23 @@ DROP TABLE IF EXISTS `goods`;
 CREATE TABLE `goods` (
   `pid` int(10) NOT NULL AUTO_INCREMENT,
   `inlibrary_id` int(10) DEFAULT NULL,
-  `pname` varchar(255) DEFAULT NULL,
+  `pname` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   `pnum` int(255) DEFAULT NULL,
   `area` varchar(255) DEFAULT NULL,
   `room` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1578948963 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1578948972 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES ('1578948944', '1151864949', '奶酪', '14', 'A2', '502');
-INSERT INTO `goods` VALUES ('1578948945', '1151864950', '奶酪', '14', 'A1', '102');
-INSERT INTO `goods` VALUES ('1578948946', '1151864950', '起司', '14', 'A2', '205');
+INSERT INTO `goods` VALUES ('1578948944', '1151864949', '奶酪', '12', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948945', '1151864950', '奶酪', '14', 'A2', '102');
+INSERT INTO `goods` VALUES ('1578948946', '1151864950', '起司', '14', 'A1', '101');
 INSERT INTO `goods` VALUES ('1578948947', '1151864975', '蛤蛤', '15', 'A2', '156');
-INSERT INTO `goods` VALUES ('1578948948', '1151864989', '肥宅快乐水', '123', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948948', '1151864989', '肥宅快乐水', '111', 'A2', '103');
 INSERT INTO `goods` VALUES ('1578948949', '1151864992', '肥宅快乐水', '1', 'A1', '101');
-INSERT INTO `goods` VALUES ('1578948950', '1151865012', '肥宅快乐奶茶', '1123', 'A2', '102');
+INSERT INTO `goods` VALUES ('1578948950', '1151865012', '肥宅快乐奶茶', '1123', 'A1', '101');
 INSERT INTO `goods` VALUES ('1578948951', '1151865013', '一颗赛艇', '1123', 'A2', '102');
 INSERT INTO `goods` VALUES ('1578948952', '1151865014', '二院视察', '50', 'A1', '101');
 INSERT INTO `goods` VALUES ('1578948953', '1151865015', '三个代表', '50', 'A1', '101');
@@ -51,6 +51,15 @@ INSERT INTO `goods` VALUES ('1578948959', '1151865021', '九十大寿', '561', '
 INSERT INTO `goods` VALUES ('1578948960', '1151865022', '十全长者', '65', 'A3', '405');
 INSERT INTO `goods` VALUES ('1578948961', '1151865023', '张宝华', '1', 'A1', '101');
 INSERT INTO `goods` VALUES ('1578948962', '1151865024', '董先生', '123', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948963', '1151865029', '野子', '1', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948964', '1151865030', 'the', '1', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948965', '1151865031', '百叶', '1', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948966', '1151865032', '牛百叶', '16', 'A2', '102');
+INSERT INTO `goods` VALUES ('1578948967', '1151865032', '水', '1', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948968', '1151865032', '1', '1', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948969', '1151865033', '千张', '1', 'A1', '101');
+INSERT INTO `goods` VALUES ('1578948970', '1151864989', '肥宅快乐水', '12', 'A2', '103');
+INSERT INTO `goods` VALUES ('1578948971', '1151864989', '肥宅快乐水', '12', 'A2', '103');
 
 -- ----------------------------
 -- Table structure for inlibrary
@@ -65,7 +74,7 @@ CREATE TABLE `inlibrary` (
   `in_date` datetime DEFAULT NULL,
   `in_user` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1151865026 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1151865034 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of inlibrary
@@ -87,8 +96,8 @@ INSERT INTO `inlibrary` VALUES ('1151865020', '八门外语', '165', 'A2', '102'
 INSERT INTO `inlibrary` VALUES ('1151865021', '九十大寿', '561', 'A4', '205', '2018-08-17 11:12:52', '九十大寿');
 INSERT INTO `inlibrary` VALUES ('1151865022', '十全长者', '65', 'A3', '405', '2018-08-17 11:13:01', '十全长者');
 INSERT INTO `inlibrary` VALUES ('1151865023', '张宝华', '1', 'A1', '101', '2018-08-17 11:51:52', '张宝华');
-INSERT INTO `inlibrary` VALUES ('1151865024', '董先生', '123', 'A1', '101', '2018-08-20 14:42:44', null);
 INSERT INTO `inlibrary` VALUES ('1151865025', ' 梁逸峰', '2', 'A1', '102', '2018-08-23 11:02:28', '梁逸峰');
+INSERT INTO `inlibrary` VALUES ('1151865033', '千_张', '1', 'A1', '101', '2018-08-27 14:57:03', '超级管理员');
 
 -- ----------------------------
 -- Table structure for move
@@ -106,7 +115,7 @@ CREATE TABLE `move` (
   `move_time` datetime DEFAULT NULL,
   `move_user` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`mid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1184949789 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1184949805 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of move
@@ -123,6 +132,22 @@ INSERT INTO `move` VALUES ('1184949785', '1151865019', '七因国服', '50', 'A1
 INSERT INTO `move` VALUES ('1184949786', '1151865020', '八门外语', '165', 'A2', '113', 'A2', '102', '2018-08-17 11:12:35', '八门外语');
 INSERT INTO `move` VALUES ('1184949787', '1151865021', '九十大寿', '561', 'A4', '320', 'A4', '205', '2018-08-17 11:12:52', '九十大寿');
 INSERT INTO `move` VALUES ('1184949788', '1151865022', '十全长者', '65', 'A3', '105', 'A3', '405', '2018-08-17 11:13:01', '十全长者');
+INSERT INTO `move` VALUES ('1184949789', '1578948944', '奶酪', '14', 'A2', '502', 'A1', '101', '2018-08-27 16:26:06', null);
+INSERT INTO `move` VALUES ('1184949790', '1578948944', '奶酪', '14', 'A1', '101', 'A1', '102', '2018-08-27 16:27:15', null);
+INSERT INTO `move` VALUES ('1184949791', '1578948944', '奶酪', '14', 'A1', '102', 'A1', '103', '2018-08-27 16:28:17', '超级管理员');
+INSERT INTO `move` VALUES ('1184949792', '1578948946', '起司', '14', 'A2', '205', 'A1', '101', '2018-08-27 16:29:20', '超级管理员');
+INSERT INTO `move` VALUES ('1184949793', '1578948946', '起司', '14', 'A2', '205', 'A1', '101', '2018-08-27 16:29:16', '超级管理员');
+INSERT INTO `move` VALUES ('1184949794', '1578948950', '肥宅快乐奶茶', '1123', 'A2', '102', 'A1', '101', '2018-08-27 16:31:46', '超级管理员');
+INSERT INTO `move` VALUES ('1184949795', '1578948946', '起司', '14', 'A1', '101', 'A3', '103', '2018-08-27 16:35:52', '超级管理员');
+INSERT INTO `move` VALUES ('1184949796', '1578948944', '奶酪', '14', 'A1', '103', 'A1', '101', '2018-08-27 16:39:43', '超级管理员');
+INSERT INTO `move` VALUES ('1184949797', '1578948944', '奶酪', '14', 'A1', '101', 'A3', '101', '2018-08-27 16:44:05', '超级管理员');
+INSERT INTO `move` VALUES ('1184949798', '1578948944', '奶酪', '14', 'A1', '101', 'A3', '102', '2018-08-27 16:54:31', '超级管理员');
+INSERT INTO `move` VALUES ('1184949799', '1578948944', '奶酪', '14', 'A3', '102', 'A1', '103', '2018-08-27 16:55:00', '超级管理员');
+INSERT INTO `move` VALUES ('1184949800', '1578948944', '奶酪', '14', 'A1', '103', 'A3', '103', '2018-08-27 16:57:48', '超级管理员');
+INSERT INTO `move` VALUES ('1184949801', '1578948944', '奶酪', '14', 'A3', '103', 'A3', '101', '2018-08-27 17:07:21', '超级管理员');
+INSERT INTO `move` VALUES ('1184949802', '1578948948', '肥宅快乐水', '12', 'A1', '101', 'A2', '103', '2018-08-27 17:22:26', '超级管理员');
+INSERT INTO `move` VALUES ('1184949803', '1578948948', '肥宅快乐水', '12', 'A1', '101', 'A2', '103', '2018-08-27 17:22:26', '超级管理员');
+INSERT INTO `move` VALUES ('1184949804', '1578948944', '奶酪', '1', 'A3', '101', 'A1', '101', '2018-08-27 17:24:37', '超级管理员');
 
 -- ----------------------------
 -- Table structure for outlibrary
@@ -130,6 +155,7 @@ INSERT INTO `move` VALUES ('1184949788', '1151865022', '十全长者', '65', 'A3
 DROP TABLE IF EXISTS `outlibrary`;
 CREATE TABLE `outlibrary` (
   `pid` int(10) NOT NULL AUTO_INCREMENT,
+  `goods_id` int(10) DEFAULT NULL,
   `pname` varchar(255) DEFAULT NULL,
   `pnum` int(255) DEFAULT NULL,
   `area` varchar(255) DEFAULT NULL,
@@ -137,34 +163,36 @@ CREATE TABLE `outlibrary` (
   `out_date` datetime DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `out_user` varchar(255) DEFAULT NULL,
-  `goods_id` int(10) DEFAULT NULL,
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1156498518 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1156498522 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of outlibrary
 -- ----------------------------
-INSERT INTO `outlibrary` VALUES ('1156498495', '一颗赛艇', '1', 'A3', '101', '2018-08-17 14:36:46', '科院超市', '一颗赛艇', '1151865013');
-INSERT INTO `outlibrary` VALUES ('1156498496', '二院视察', '2', 'A3', '101', '2018-08-17 14:36:49', '科院超市', '二院视察', '1151865014');
-INSERT INTO `outlibrary` VALUES ('1156498497', '三个代表', '3', 'A3', '101', '2018-08-17 14:36:52', '科院超市', '三个代表', '1151865015');
-INSERT INTO `outlibrary` VALUES ('1156498498', '四次起身', '4', 'A3', '101', '2018-08-17 14:36:55', '科院超市', '四次起身', '1151865016');
-INSERT INTO `outlibrary` VALUES ('1156498499', '无可奉告', '5', 'A3', '101', '2018-08-17 14:36:58', '科院超市', '无可奉告', '1151865017');
-INSERT INTO `outlibrary` VALUES ('1156498500', '六月水柜', '6', 'A3', '101', '2018-08-17 14:37:01', '科院超市', '六月水柜', '1151865018');
-INSERT INTO `outlibrary` VALUES ('1156498501', '七因祸福', '7', 'A3', '101', '2018-08-17 14:37:04', '科院超市', '七因祸福', '1151865019');
-INSERT INTO `outlibrary` VALUES ('1156498502', '八门外语', '8', 'A3', '101', '2018-08-17 14:37:06', '科院超市', '八门外语', '1151865020');
-INSERT INTO `outlibrary` VALUES ('1156498503', '九十大寿', '9', 'A3', '101', '2018-08-17 14:37:08', '科院超市', '九十大寿', '1151865021');
-INSERT INTO `outlibrary` VALUES ('1156498504', '十全长者', '10', 'A3', '101', '2018-08-17 14:37:16', '科院超市', '十全长者', '1151865022');
-INSERT INTO `outlibrary` VALUES ('1156498505', '张宝华', null, null, null, '2018-08-17 16:23:59', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498508', '彭定康', null, null, null, '2018-08-17 16:24:07', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498509', '彭定康', null, null, null, '2018-08-17 16:24:09', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498510', '彭定康', null, null, null, '2018-08-17 16:24:12', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498511', '彭定康', null, null, null, '2018-08-17 16:24:14', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498512', '彭定康', null, null, null, '2018-08-17 16:24:17', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498513', '彭定康', null, null, null, '2018-08-17 16:24:22', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498514', '彭定康', null, null, null, '2018-08-17 16:24:25', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498515', '彭定康', null, null, null, '2018-08-17 16:24:27', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498516', '彭定康', null, null, null, '2018-08-17 16:24:35', null, null, null);
-INSERT INTO `outlibrary` VALUES ('1156498517', '彭定康', null, null, null, '2018-08-17 16:24:38', null, null, null);
+INSERT INTO `outlibrary` VALUES ('1156498495', '1151865013', '一颗赛艇', '1', 'A3', '101', '2018-08-17 14:36:46', '科院超市', '一颗赛艇');
+INSERT INTO `outlibrary` VALUES ('1156498496', '1151865014', '二院视察', '2', 'A3', '101', '2018-08-17 14:36:49', '科院超市', '二院视察');
+INSERT INTO `outlibrary` VALUES ('1156498497', '1151865015', '三个代表', '3', 'A3', '101', '2018-08-17 14:36:52', '科院超市', '三个代表');
+INSERT INTO `outlibrary` VALUES ('1156498498', '1151865016', '四次起身', '4', 'A3', '101', '2018-08-17 14:36:55', '科院超市', '四次起身');
+INSERT INTO `outlibrary` VALUES ('1156498499', '1151865017', '无可奉告', '5', 'A3', '101', '2018-08-17 14:36:58', '科院超市', '无可奉告');
+INSERT INTO `outlibrary` VALUES ('1156498500', '1151865018', '六月水柜', '6', 'A3', '101', '2018-08-17 14:37:01', '科院超市', '六月水柜');
+INSERT INTO `outlibrary` VALUES ('1156498501', '1151865019', '七因祸福', '7', 'A3', '101', '2018-08-17 14:37:04', '科院超市', '七因祸福');
+INSERT INTO `outlibrary` VALUES ('1156498502', '1151865020', '八门外语', '8', 'A3', '101', '2018-08-17 14:37:06', '科院超市', '八门外语');
+INSERT INTO `outlibrary` VALUES ('1156498503', '1151865021', '九十大寿', '9', 'A3', '101', '2018-08-17 14:37:08', '科院超市', '九十大寿');
+INSERT INTO `outlibrary` VALUES ('1156498504', '1151865022', '十全长者', '10', 'A3', '101', '2018-08-17 14:37:16', '科院超市', '十全长者');
+INSERT INTO `outlibrary` VALUES ('1156498505', null, '张宝华', null, null, null, '2018-08-17 16:23:59', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498509', null, '彭定康', null, null, null, '2018-08-17 16:24:09', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498510', null, '彭定康', null, null, null, '2018-08-17 16:24:12', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498511', null, '彭定康', null, null, null, '2018-08-17 16:24:14', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498512', null, '彭定康', null, null, null, '2018-08-17 16:24:17', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498513', null, '彭定康', null, null, null, '2018-08-17 16:24:22', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498514', null, '彭定康', null, null, null, '2018-08-17 16:24:25', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498515', null, '彭定康', null, null, null, '2018-08-17 16:24:27', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498516', null, '彭定康', null, null, null, '2018-08-17 16:24:35', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498517', null, '彭定康', null, null, null, '2018-08-17 16:24:38', null, null);
+INSERT INTO `outlibrary` VALUES ('1156498518', '1578948944', '奶酪', '1', 'A1', '101', '2018-08-28 10:45:48', '撒大大大', '超级管理员');
+INSERT INTO `outlibrary` VALUES ('1156498519', '1578948972', '奶酪', '1', 'A1', '101', '2018-08-28 10:46:26', '我爱普股份股票符合', '超级管理员');
+INSERT INTO `outlibrary` VALUES ('1156498520', '1578948972', '奶酪', '1', 'A1', '101', '2018-08-28 10:46:40', '我爱普股份股票符合', '超级管理员');
+INSERT INTO `outlibrary` VALUES ('1156498521', '1578948972', '奶酪', '1', 'A1', '101', '2018-08-28 10:50:21', '就要付邮费贵', '超级管理员');
 
 -- ----------------------------
 -- Table structure for user
